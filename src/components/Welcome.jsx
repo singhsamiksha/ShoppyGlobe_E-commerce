110badf68afd2e35d4fe7a646dc948c4fe7c1b34
singhsamiksha <singhsamiksha877@gmail.com>
2025-01-14 14:23:31 +0530

    Welcome page is added

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../stylesheets/Welcome.css';
+
+function Welcome() {
+  return (
+    <>
+      <div className="welcome_page">
+        <div className="title">
+          <h1>Welcome to ShoppyGlobe</h1>
+          <p>Your one-stop shop for all your needs!</p>
+          <Link to="/products">
+            <button>Start Shopping</button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Welcome;
