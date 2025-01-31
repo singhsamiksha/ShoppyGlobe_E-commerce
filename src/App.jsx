f0a232afe72bac04fe914bcac2ce6cbf770618f0
Samiksha <singhsamiksha877@gmail.com>
2025-01-31 13:52:57 +0530

    Added lazy loading for routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import React, { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './components/common/Header';
 import './App.css';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Header from './components/common/Header';
+
+const Welcome = lazy(() => import('./components/Welcome'));
+const Product = lazy(() => import('./components/ProductView'));
+const ProductDetails = lazy(() => import('./components/common/products/ProductDetail'));
+const Cart = lazy(() => import('./components/CartView'));
+const PlaceOrder = lazy(() => import('./components/PlaceOrderView'));
+const OrderPage = lazy(() => import('./components/OrderPageView'));
+const NotFound = lazy(() => import('./components/common/NotFound'));
+
+const AppPage = ({ children }) => (
+  <>
+    <Header />
+    {children}
+  </>
+);
+
+AppPage.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const appRoutes = [
+  { path: "/", element: <AppPage><Welcome /></AppPage> },
+
+  { path: "/products", element: <AppPage><Product /> </AppPage> },
+  { path: "/products/:id", element: <AppPage><ProductDetails /> </AppPage> },
+
+  { path: "/cart", element: <AppPage><Cart /> </AppPage> },
+  { path: "/checkout", element: <AppPage><PlaceOrder /> </AppPage>, },
+  { path: "/order", element: <AppPage><OrderPage /> </AppPage> },
+  { path: "*", element:  <AppPage><NotFound /></AppPage> }
+]
 
 const App = () => (
   <div className="app">
-    <Header />
-    <Suspense fallback={<div>Loading...</div>}>
-      <Outlet />
-    </Suspense>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {
+            appRoutes.map((route, key) => (
+              <Route key={`${key}-app-routes`} path={route.path} element={route.element} />
+            ))
+          }
+        </Routes>
+      </Suspense>
+    </Router>
   </div>
 );
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,46 +1,16 @@
+import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
-import Welcome from './components/Welcome';
-import AllProduct from './components/ProductView.jsx';
 import store from './utilies/store.js';
-import Cart from './components/CartView.jsx';
-import ProductDetails from './components/common/products/ProductDetail.jsx';
-import NotFound from './components/common/NotFound.jsx';
-import PlaceOrder from './components/PlaceOrderView.jsx';
-import OrderPage from './components/OrderPageView.jsx';
-import 'font-awesome/css/font-awesome.min.css';
 
-const appRoutes = [
-    { path: "/", element: <Welcome />, },
-    { path: "/products", element: <AllProduct /> },
-    { path: "/cart", element: <Cart /> },
-    { path: "/checkout", element: <PlaceOrder />, },
-    { path: "/order", element: <OrderPage />, },
-    { path: "/products/:id", element: <ProductDetails /> },
-]
-
-const appRouter = createBrowserRouter([
-    ...appRoutes.map((route) => {
-        return {
-            path: "/",
-            element: <App />,
-            children: [route],
-        };
-    }),
-    {
-        path: "*",
-        element: <NotFound />,
-    }
-]);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
-            <RouterProvider router={appRouter} />
+            <App />
         </Provider>
     </StrictMode>
 );
